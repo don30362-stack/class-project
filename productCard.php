0d098d4de0d14b3cf6ae3435a0074fbe59a026b3
Wang <don30362@gmail.com>
2026-08-18 16:15:14 +0800

    modify productDetail

--- productCard.php
+++ productCard.php
@@ -29,17 +29,18 @@ $i = 1;
         <?php if ($i % 4 == 1) { ?>
             <div class="row text-center">
             <?php } ?>
-
-            <div class="card col-6 col-md-3">
-                <a href="productDetail.php?p_id=<?php echo $pList01_Rows['p_id']; ?>">
-                    <img src="./product_img/<?= $pList01_Rows['img_file'] ?>" class="card-img-top" alt="<?= $pList01_Rows['p_name'] ?>" title="<?= $pList01_Rows['p_name'] ?>">
-                </a>
-                <div class="card-body">
-                    <h5 class="card-title"><?= $pList01_Rows['p_name'] ?></h5>
-                    <p class="card-text"><?= $pList01_Rows['p_intro'] ?></p>
-                    <p class="card-text">NT<?= $pList01_Rows['p_price'] ?></p>
-                    <a href="productDetail.php?p_id=<?php echo $pList01_Rows['p_id']; ?>" class="btn btn-primary">更多資訊</a>
-                    <a href="#" class="btn btn-success">放購物車</a>
+            <div class="col-6 col-md-3">
+                <div class="card h-100 border rounded-0 shadow-sm">
+                    <a href="productDetail.php?p_id=<?php echo $pList01_Rows['p_id']; ?>">
+                        <div class="ratio ratio-1x1 bg-light">
+                            <img src="./product_img/<?= $pList01_Rows['img_file'] ?>" class="card-img-top" alt="<?= $pList01_Rows['p_name'] ?>" title="<?= $pList01_Rows['p_name'] ?>">
+                        </div>
+                    </a>
+                    <div class="card-body p-2 p-md-3">
+                        <h5 class="card-title"><?= $pList01_Rows['p_name'] ?></h5>
+                        <p class="card-text"><?= $pList01_Rows['p_intro'] ?></p>
+                        <p class="card-text">NT<?= $pList01_Rows['p_price'] ?></p>
+                    </div>
                 </div>
 
             </div>
