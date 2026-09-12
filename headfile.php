59ebe5ee7d5d1484ec363a04361d5bd922804f0a
Wang <don30362@gmail.com>
2026-09-12 15:05:50 +0800

    refactor: organize CSS by component

--- headfile.php
+++ headfile.php
@@ -3,4 +3,21 @@
 <title>Home Fitness</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.0/css/all.min.css">
-<link rel="stylesheet" href="website_p01.css">
+<!-- 樣式順序與 website_p01.css 保持一致，詳見 docs/styles.md。 -->
+<link rel="stylesheet" href="css/navbar.css">
+<link rel="stylesheet" href="css/carousel.css">
+<link rel="stylesheet" href="css/breadcrumb.css">
+<link rel="stylesheet" href="css/recommend.css">
+<link rel="stylesheet" href="css/product-filter.css">
+<link rel="stylesheet" href="css/product-card.css">
+<link rel="stylesheet" href="css/pagination.css">
+<link rel="stylesheet" href="css/product-detail.css">
+<link rel="stylesheet" href="css/categories.css">
+<link rel="stylesheet" href="css/recommendation-section.css">
+<link rel="stylesheet" href="css/why-us.css">
+<link rel="stylesheet" href="css/footer-cta.css">
+<link rel="stylesheet" href="css/footer.css">
+<link rel="stylesheet" href="css/cart.css">
+<link rel="stylesheet" href="css/checkout.css">
+<link rel="stylesheet" href="css/login.css">
+<link rel="stylesheet" href="css/register.css">
